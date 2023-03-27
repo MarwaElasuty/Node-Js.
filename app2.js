8b95636a0df424624b424e55b9a15926cdd0dd30
MarwaElasuty <52058731+MarwaElasuty@users.noreply.github.com>
2023-03-27 03:17:07 +0200

    Delete app2.js

--- app2.js
+++
@@ -1,8 +0,0 @@
-var Customer=require('./Q2');
-try{
-    var getString=Customer.getAge('Marwa',new Date(2000,7,13));
-    console.log(getString);
-}
-catch(error){
-    console.error(error.message)
-};
